67041d407994fbde42db00d411b50cff5448b7a5
Peggy Gillette <dark_aoekae@yahoo.com>
2022-07-07 10:51:45 -0500

    Created basic files and styling, basic html structure for main page

---
+++ script.js
@@ -0,0 +1,10 @@
+const scripthelper = require("./scripthelper.js")
+
+window.addEventListener("load", function(){
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        for (let index=0; index < scripthelper.calculatorArray.length; index++) {
+            
+        }
+    });
+    });
